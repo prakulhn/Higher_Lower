45e73a86db820130abb8fd554dd0e71bceb055f2
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-11-26 16:58:08 +0530

    Create game_database.py

---
+++ game_database.py
@@ -0,0 +1,50 @@
+candidates_data = [
+    {
+        'name': 'Prakul H N',
+        'followers_count': 150,
+        'description': 'Student',
+        'location': 'India'
+    },
+    {
+        'name': 'Jayanthi Khatrilamba',
+        'followers_count': 1450000,
+        'description': 'Youtuber',
+        'location': 'India'
+    },
+    {
+        'name': 'Ratan Tata',
+        'followers_count': 8900000,
+        'description': 'Industrialist, Philantrophist',
+        'location': 'India'
+    },
+    {
+        'name': 'Arvind Khejriwal',
+        'followers_count': 1700000,
+        'description': 'Politician, Chief Minister',
+        'location': 'India'
+    },
+    {
+        'name': 'Christiano Ronaldo ',
+        'followers_count': 612000000,
+        'description': 'Sportsman, Footballer',
+        'location': 'Portugal'
+    },
+    {
+        'name': 'Pawan Kumar Sehrawat',
+        'followers_count': 966000,
+        'description': 'Sportsman, Kabaddi Player',
+        'location': 'India'
+    },
+    {
+        'name': 'Rishi Sunak',
+        'followers_count': 3000000,
+        'description': 'Prime Minister, Politician',
+        'location': 'United Kingdom'
+    },
+    {
+        'name': 'Sundar Pichai',
+        'followers_count': 2900000,
+        'description': 'Chief Executive Officer, Google',
+        'location': 'California'
+    },
+]
